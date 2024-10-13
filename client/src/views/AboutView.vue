ef260425fddb6409f8998b6acf5c5e949b9d98d3
DominikDuda <Dominik.Duda96@gmx.de>
2024-10-13 19:34:45 +0200

    added about page content

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -3,14 +3,69 @@
 </script>
 
 <template>
-  <section class="content">
-    
-  </section>
+  <div class="content">
+    <section>
+      <h1>About this website</h1>
+      <p>This website enables you to select a random agent, map, or strategy for your next Valorant game.</p>
+      <br>
+      <p>Data utilized here is provided by <a class="link" href="https://valorantapi.com"
+          target="_blank">valorantapi.com</a>.</p>
+    </section>
+
+    <section>
+      <h1>Buy me a coffee</h1>
+      <p>If you like this project and want to support me, consider buying me a coffee.</p>
+      <a class="buy-coffee" href="https://www.buymeacoffee.com/flashrex" target="_blank" rel="noopener noreferrer">
+        <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee"
+          style="height: 60px !important;width: 217px !important;" />
+      </a>
+    </section>
+
+    <section>
+      <h1>Contribute</h1>
+      <p>If you want to contribute to this project, you can find the source code on <a class="link"
+          href="https://github.com/Flashrex/valorant_randomizer" target="_blank" rel="noopener noreferrer">GitHub</a>.
+      </p>
+    </section>
+
+    <section>
+      <h1>Disclaimer</h1>
+      <p>Valorant is a registered trademark of Riot Games, Inc. </p>
+      <p>Riot Games, Inc. does not endorse or sponsor this project.</p>
+    </section>
+
+    <section>
+      <h1>Privacy Policy</h1>
+      <p>This website does not collect any personal data.</p>
+    </section>
+
+  </div>
 </template>
 
 <style scoped>
 .content {
-  height: inherit;
-  width: inherit;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  height: 80vh;
+  color: var(--color-text);
+
+  gap: 2rem;
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  width: 80vw;
+  max-width: 800px;
+  padding: 1rem;
+  border-radius: 10px;
+}
+
+.buy-coffee {
+  margin-top: 1rem;
 }
 </style>
