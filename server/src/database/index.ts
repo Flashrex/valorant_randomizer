f74b7f6e28f9c82cc6f5685f3bed09f45c71a185
DominikDuda <Dominik.Duda96@gmx.de>
2024-10-27 12:37:23 +0100

    fixed error handling / crashes

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -6,11 +6,12 @@ import createTables from './model';
 export default class Database {
 
     private static _instance: Database;
-    private connection: mysql.Connection;
+    private connection?: mysql.Connection;
+    private config: mysql.ConnectionConfig;
 
     constructor() {
 
-        const config = {
+        this.config = {
             host: ENV.DATABASE_HOST,
             port: ENV.DATABASE_PORT ? parseInt(ENV.DATABASE_PORT) : 4406,
             user: ENV.DATABASE_USER,
@@ -18,7 +19,7 @@ export default class Database {
             database: ENV.DATABASE_NAME,
         };
 
-        this.connection = mysql.createConnection(config);
+        this.createConnection();
     }
 
     public static get instance() : Database {
@@ -26,12 +27,25 @@ export default class Database {
     }
 
     public get isConnected() : boolean {
-        return this.connection.state === 'authenticated';
+        return this.connection?.state === 'authenticated';
+    }
+
+    private createConnection() {
+        this.connection = mysql.createConnection(this.config);
+
+        this.connection.on('error', (err) => {
+            Logger.error('database', `Database connection error: ${err}`);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                this.handleDisconnect();
+            } else {
+                throw err;
+            }
+        });
     }
 
     connect(attempts = 0): Promise<void> {
         return new Promise((resolve, reject) => {
-            this.connection.connect((err) => {
+            this.connection?.connect((err) => {
                 if (err) {
                     if (attempts < 3) {
                         Logger.log('database', 'Retrying connection...');
@@ -53,10 +67,23 @@ export default class Database {
     }
 
     query(sql: string, values: any, callback?: (err: mysql.MysqlError | null, results?: any, fields?: mysql.FieldInfo[]) => void) {
-        this.connection.query(sql, values, callback);
+        if(!this.isConnected) return;
+        this.connection?.query(sql, values, callback);
     }
 
     get state() {
-        return this.connection.state;
+        return this.connection?.state;
+    }
+
+    private handleDisconnect() {
+        Logger.log('database', 'Attempting to reconnect to the database...');
+        const attemptReconnect = () => {
+            this.createConnection();
+            this.connect().catch((error) => {
+                Logger.error('database', 'Failed to reconnect to database, retrying...');
+                setTimeout(attemptReconnect, 5000);
+            });
+        };
+        attemptReconnect();
     }
 }
